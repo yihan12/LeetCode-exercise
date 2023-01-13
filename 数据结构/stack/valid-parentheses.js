205c5a639e1a2fa5946e202e2338b35f2e1b92c6
yihan12 <44987698+yihan12@users.noreply.github.com>
2023-01-13 17:06:48 +0800

    Update valid-parentheses.js

--- 数据结构/stack/valid-parentheses.js
+++ 数据结构/stack/valid-parentheses.js
@@ -1,3 +1,25 @@
-const isValid = function(s){
-    
-}
+ const isValid = function(s){
+    if(s.length==0){
+      return false
+    }
+    const sVar = new Map([
+      ['(',')'],
+      ['[',']'],
+      ['{','}'],
+    ])
+    const stack = []
+    for(let i = 0; i < s.length; i++){
+      if(sVar.has(s[i])){
+        stack.push(s[i])
+      }else if(sVar.get(stack[stack.length-1])==s[i]){
+        stack.pop()
+      }else{
+        stack.push(s[i])
+      }
+    }
+    if(stack.length==0){
+      return true
+    }else{
+      return false
+    }
+  }
